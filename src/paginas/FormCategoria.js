c02ef9293dcfd9414c9e3fcb4e5cb934898f933a
mateusdariva <mateus.dariva@gmail.com>
2025-05-13 21:59:24 -0300

    Criação das paginas padrões

---
+++ src/paginas/FormCategoria.js
@@ -0,0 +1,10 @@
+export default function FormCategoria () {
+    return(
+        <>
+        <h1>Cadastro de Categoria</h1>
+        </> 
+
+    );
+
+
+};
---
+++ src/paginas/ListaCategoria.js
@@ -0,0 +1,52 @@
+import TituloLista from "../componentes/TítuloLista";
+
+export default function ListaCategoria () {
+    return(
+        <>
+        <TituloLista titulo="Categorias" descricao="Gerencie aqui as categorias dos livros da biblioteca" rota="/categoria"/>
+
+        <>
+  {/*DADOS*/}
+  <div className="row">
+    <div className="col">
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  {/*FIM DADOS*/}
+</>
+
+        </> 
+        
+
+    );
+
+
+};
